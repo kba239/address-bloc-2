9cadb9d36772d854913aa28a73923653704ecec0
kba239 <adams.kate.b@gmail.com>
2018-01-11 11:34:13 -0800

    Initial Commit

---
+++ address_bloc.rb
@@ -0,0 +1,9 @@
+require_relative 'controllers/menu_controller'
+require 'bloc_record'
+
+BlocRecord.connect_to("db/address_bloc.sqlite")
+
+menu = MenuController.new
+system "clear"
+puts "Welcome to AddressBloc!"
+menu.main_menu
---
+++ models/address_book.rb
@@ -0,0 +1,21 @@
+require_relative 'entry'
+require "csv"
+require 'bloc_record/base'
+
+class AddressBook < BlocRecord::Base
+  attr_reader :entries
+
+  def add_entry(name, phone_number, email)
+    Entry.create(name: name, phone_number: phone, email: email, address_book_id: self.id)
+  end
+
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
+  end
+
+end
